7aa3ef8da2c65361dda98fb9e184aaade64f5fbd
Thang Nguyen <thangnd@stageit.app>
2023-11-02 12:49:21 +0700

    WIP

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,5 +4,9 @@ import betiuService from "../routes/BetiuService"
 const router = express.Router();
 
 router.use("/betiu-services", betiuService);
+router.get("health-check", (req, res) => {
+    res.status(200)
+    res.send();
+});
 
 export default router;
